5e22c3ad6bea76110db95de5c6b04a8c29cda3e6
jourdanmauricio <jourdanmauricio@gmail.com>
2023-10-23 19:27:31 -0300

    Add hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,23 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  image: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.cloudinary.com',
+      },
+    ],
+  },
+  site: 'https://jourdanmauricio.github.io',
+  base: '/libreria-alfa',
+  vite: {
+    ssr: {
+      external: ['svgo'],
+    },
+  },
 });
