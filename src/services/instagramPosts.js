ab982855284faae2c3b7706db6d992e1201925a1
jourdanmauricio <jourdanmauricio@gmail.com>
2023-11-07 23:32:43 -0300

    .

--- src/services/instagramPosts.js
+++ src/services/instagramPosts.js
@@ -2,7 +2,7 @@ const BACKEND = import.meta.env.PUBLIC_BACKEND;
 const URL = import.meta.env.PUBLIC_URL;
 
 export const getInstagramPosts = async () => {
-  let networks = [];
+  let instagramPosts = [];
   try {
     const res = await fetch(`${BACKEND}/instagram-posts`, {
       headers: { 'Content-Type': 'application/json', url: URL },
@@ -10,10 +10,11 @@ export const getInstagramPosts = async () => {
     if (res.status !== 200) {
       console.log('Error GET instagram: ', res.status, res.statusText);
     }
-    networks = await res.json();
+    instagramPosts = await res.json();
   } catch (error) {
     console.log('ERROR DE CONEXION. Contacte al administrador!!!!', error);
   }
 
-  return networks[0];
+  console.log('instagramPosts', instagramPosts);
+  return instagramPosts;
 };
